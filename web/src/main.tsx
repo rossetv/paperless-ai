bf37c96f1eeeeef64771e9e3127185d251c9f5f9
Vilmar Rosset <vrosset@ebay.com>
2026-05-20 12:37:39 +0100

    feat(web): add Apple-derived design tokens and global styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './styles/global.css';
 import App from './App';
 
 const queryClient = new QueryClient({
